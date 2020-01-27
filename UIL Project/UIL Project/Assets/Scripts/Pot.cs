2313d601a772ee9e76ce8cb1bc757fdda3709a12
Typical-Joey <jtcapocci@yahoo.com>
2020-01-26 21:54:25 -0600

    Adding Game

---
+++ UIL Project/UIL Project/Assets/Scripts/Pot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void Smash() 
+    {
+        ani.SetBool("Smashed", true);
+        StartCoroutine(breakco());
+    }
+
+    IEnumerator breakco() 
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
